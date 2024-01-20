44bd534a5099cd88d42a1cd826eb5f4d7714bfc5
sktrader <kswapnil55@gmail.com>
2024-01-20 16:30:10 +0530

    swager implementation

---
+++ src/main/java/com/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Ecommerce App APIs")
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.ecommerce.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Ecommerce Api Details")
+				.description("Spring Rest Apis Doc")
+				.version("1.0")
+				.build();
+	}
+
+}
